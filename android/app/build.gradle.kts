69a48e034077df80de779f91602abbb868e11df4
Vithushan <vithushan@silverlineit.co>
2025-05-01 17:54:59 +0530

    feat: Set minimum SDK version to 23 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
         applicationId = "com.example.chillfix_provider"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        //  flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
